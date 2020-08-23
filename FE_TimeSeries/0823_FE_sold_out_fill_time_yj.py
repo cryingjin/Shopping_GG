fc157b994eeaa2b55efd45c4665eb55cccd219d2
Yejin Lee <41279475+cryingjin@users.noreply.github.com>
2020-08-23 20:35:33 +0900

    Add FE sold_out_fill_time 함수 추가 0823

---
+++ FE_TimeSeries/0823_FE_sold_out_fill_time_yj.py
@@ -0,0 +1,8 @@
+# 0823
+# 조기매진 함수 : 시간의 nan 값 채워주기 전에 생성할 것
+def sold_out_fill_time(df):
+    # 조기매진 feature 생성
+    df['조기매진'] = sale_data['노출(분)'].map(lambda x: 1 if ((x < 20) | (x > 20) & (x < 30)) else 0)     # 20분 이하, 20-30 분 사이
+    # 방송일시의 nan 값 채워주는 부분
+    df["노출(분)"] = df["노출(분)"].fillna(method='ffill')
+    return df
