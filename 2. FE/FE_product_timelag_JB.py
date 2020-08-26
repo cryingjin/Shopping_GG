c30f2acf3ecb2d3e7fc5b497f7f2250ba515dc1d
jbeen2 <43749571+jbeen2@users.noreply.github.com>
2020-08-26 23:45:10 +0900

    FE.py

--- 2. FE/FE_product_timelag_JB.py
+++ 2. FE/FE_product_timelag_JB.py
@@ -26,16 +26,16 @@ def sales_count(df):
 
 
 # 동일상품 시간차
-def product_timelag(df):
-    df["duration"] = df.groupby(["NEW상품명"])["방송일시"].diff()
-
-    df["duration"] = df["duration"].fillna(0)
-
-    # days, hours, minutes -> minutes 기준으로 합쳐 주기로 함 !
-    time = df['duration'].dt.components[['days', 'hours', 'minutes']]
-    time["동일상품시간차"] = time['days'] * 60 * 24 + time['hours'] * 60 + time['minutes']
-
-    df = pd.concat([df, time], axis=1)
-    df = df.drop(['duration', 'days', 'hours', 'minutes'], axis=1)
-
-    return df
+# def product_timelag(df):
+#     df["duration"] = df.groupby(["NEW상품명"])["방송일시"].diff()
+#
+#     df["duration"] = df["duration"].fillna(0)
+#
+#     # days, hours, minutes -> minutes 기준으로 합쳐 주기로 함 !
+#     time = df['duration'].dt.components[['days', 'hours', 'minutes']]
+#     time["동일상품시간차"] = time['days'] * 60 * 24 + time['hours'] * 60 + time['minutes']
+#
+#     df = pd.concat([df, time], axis=1)
+#     df = df.drop(['duration', 'days', 'hours', 'minutes'], axis=1)
+#
+#     return df
