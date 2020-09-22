6364184809d65ef42f131efab7ade5e586a32077
jbeen2 <dlwoqls4323@gmail.com>
2020-09-23 00:58:11 +0900

    update

--- Feature Engineering/sales_count.py
+++
@@ -1,8 +0,0 @@
-# 동일 상품 / 브랜드 총 판매 횟수 
-def sales_count(df):
-    df["상품총판매횟수"] = df.groupby(["NEW상품명"])["방송일시"].transform('size')
-    df["브랜드총판매횟수"] = df.groupby(["브랜드"])["방송일시"].transform('size')
-
-    df["브랜드총판매횟수"] = df["브랜드총판매횟수"].fillna(0)
-
-    return df
